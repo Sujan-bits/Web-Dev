6e6ffd77b7c739f99ead299759b604ed88d1cdc1
Sujan-bits <2021wb86054@wilp.bits-pilani.ac.in>
2025-05-05 01:34:00 +0530

    Added script in Java branch-v2

--- testScript.js
+++ testScript.js
@@ -5,3 +5,19 @@ if (letters.test(firstname) == false)
       document.getElementById("fname").focus();  
       return false;  
      }
+     
+     if(lastname == "" || lastname == null)
+        {
+         alert("Last name should not be empty");
+         document.getElementById("lname").focus();  
+         return false;  
+        }
+       
+       if (password.length<6)
+       {  
+         
+         alert("Password can't be less than 6 characters");
+         document.getElementById("pass").focus();  
+         return false;  
+        }
+   
